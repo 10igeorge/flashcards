6d37d8d0ee2594e69701edc34676b76e722c89ae
Isabelle George and Jason Awbrey <10izzygeorge@gmail.com>
2016-01-11 15:39:08 -0800

    Change toggle to FadeToggle

--- js/myjs.js
+++ js/myjs.js
@@ -1,27 +1,27 @@
 $(function() {
   $('#modulo').click(function() {
-    $('.init_hidden').filter('#moduloDef, #moduloDefTitle').toggle();
+    $('.init_hidden').filter('#moduloDef, #moduloDefTitle').fadeToggle();
     $('.init_hidden').not('#moduloDef, #moduloDefTitle').hide();
   });
 
   $('#alert').click(function() {
-    $('.init_hidden').filter('#alertDef, #alertDefTitle').toggle();
+    $('.init_hidden').filter('#alertDef, #alertDefTitle').fadeToggle();
     $('.init_hidden').not('#alertDef, #alertDefTitle').hide();
   });
   $('#plusEquals').click(function() {
-    $('.init_hidden').filter('#plusEqualsDef, #plusEqualsDefTitle').toggle();
+    $('.init_hidden').filter('#plusEqualsDef, #plusEqualsDefTitle').fadeToggle();
     $('.init_hidden').not('#plusEqualsDef, #plusEqualsDefTitle').hide();
   });
   $('#string').click(function() {
-    $('.init_hidden').filter('#stringDef, #stringDefTitle').toggle();
+    $('.init_hidden').filter('#stringDef, #stringDefTitle').fadeToggle();
     $('.init_hidden').not('#stringDef, #stringDefTitle').hide();
   });
   $('#escape').click(function() {
-    $('.init_hidden').filter('#escapeDef, #escapeDefTitle').toggle();
+    $('.init_hidden').filter('#escapeDef, #escapeDefTitle').fadeToggle();
     $('.init_hidden').not('#escapeDef, #escapeDefTitle').hide();
   });
   $('#boolean').click(function() {
-    $('.init_hidden').filter('#booleanDef, #booleanDefTitle').toggle();
+    $('.init_hidden').filter('#booleanDef, #booleanDefTitle').fadeToggle();
     $('.init_hidden').not('#booleanDef, #booleanDefTitle').hide();
   });
 
