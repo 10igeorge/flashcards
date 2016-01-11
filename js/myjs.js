c03e47ea93548fe8a0fc1c46a673134d36fc5257
Isabelle George and Jason Awbrey <10izzygeorge@gmail.com>
2016-01-11 15:09:45 -0800

    Adjust toggle functionality

--- js/myjs.js
+++ js/myjs.js
@@ -1,27 +1,32 @@
 $(function() {
   $('#modulo').click(function() {
-    $('#moduloDef, #moduloDefTitle').toggle();
-});
+    $('.init_hidden').filter('#moduloDef, #moduloDefTitle').toggle();
+    $('.init_hidden').not('#moduloDef, #moduloDefTitle').hide();
+  });
 
-$('#alert').click(function() {
-  $('#alertDef, #alertDefTitle').toggle();
-});
+  $('#alert').click(function() {
+    $('.init_hidden').filter('#alertDef, #alertDefTitle').toggle();
+    $('.init_hidden').not('#alertDef, #alertDefTitle').hide();
+  });
+  $('#plusEquals').click(function() {
+    $('.init_hidden').filter('#plusEqualsDef, #plusEqualsDefTitle').toggle();
+    $('.init_hidden').not('#plusEqualsDef, #plusEqualsDefTitle').hide();
+  });
+  $('#string').click(function() {
+    $('.init_hidden').filter('#stringDef, #stringDefTitle').toggle();
+    $('.init_hidden').not('#stringDef, #stringDefTitle').hide();
+  });
+  $('#escape').click(function() {
+    $('.init_hidden').filter('#escapeDef, #escapeDefTitle').toggle();
+    $('.init_hidden').not('#escapeDef, #escapeDefTitle').hide();
+  });
+  $('#boolean').click(function() {
+    $('.init_hidden').filter('#booleanDef, #booleanDefTitle').toggle();
+    $('.init_hidden').not('#booleanDef, #booleanDefTitle').hide();
+  });
 
-$('#plusEquals').click(function() {
-  $('#plusEqualsDef, #plusEqualsDefTitle').toggle();
-});
-
-$('#string').click(function() {
-  $('#stringDef, #stringDefTitle').toggle();
-});
-
-$('#escape').click(function() {
-  $('#escapeDef, #escapeDefTitle').toggle();
-});
-
-$('#boolean').click(function() {
-  $('#booleanDef, #booleanDefTitle').toggle();
-});
-
-
+  $('li').click(function() {
+    $('li.ig-ja-bold').removeClass('ig-ja-bold');
+    $(this).addClass('ig-ja-bold');
+  });
 });
